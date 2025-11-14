bf6b939cc6a57211d903a54b65206040ab1dce42
Lewis Crawford <lewis@trunks.io>
2025-11-14 11:43:39 +0000

    Handle binary STDIN in InputCollector (#2)

--- src/input.rs
+++ src/input.rs
@@ -5,31 +5,31 @@ pub struct InputSummary {
     pub lines: usize,
     pub is_empty: bool,
     pub is_probably_binary: bool,
-    pub content: String,
+    pub content: Vec<u8>,
 }
 
 pub struct InputCollector;
 
 impl InputCollector {
     pub fn collect() -> Result<InputSummary, io::Error> {
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut buffer)?;
+        let mut content = Vec::new();
+        io::stdin().read_to_end(&mut content)?;
 
-        let bytes = buffer.as_bytes().len();
-        let lines = if buffer.is_empty() {
+        let bytes = content.len();
+        let lines = if bytes == 0 {
             0
         } else {
-            buffer.lines().count()
+            content.iter().filter(|&&byte| byte == b'\n').count() + 1
         };
         let is_empty = bytes == 0;
-        let is_probably_binary = buffer.chars().any(|character| character == '\u{0}');
+        let is_probably_binary = content.iter().any(|&byte| byte == b'\0');
 
         Ok(InputSummary {
             bytes,
             lines,
             is_empty,
             is_probably_binary,
-            content: buffer,
+            content,
         })
     }
 }
